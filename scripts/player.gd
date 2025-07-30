7d75a6b50d2ed3239e4c54d364fec2c89df57ca1
GabrielAF-Faca <gabrielafachim2@gmail.com>
2025-07-30 19:00:13 -0300

    Input, Movement e animation components adicionados

---
+++ scripts/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export_subgroup("Components")
+@export var input_component: InputComponent
+@export var movement_component: MovementComponent
+@export var animation_component: AnimationComponent
+
+func _physics_process(delta: float) -> void:
+	movement_component.handle_movement(self, input_component.direction)
+	#animation_component.handle_move_animation(input_component.direction)
+	move_and_slide()
