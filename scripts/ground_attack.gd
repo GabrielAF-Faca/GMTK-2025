49b6520bf042099f27c6843983a9c11fccbbb6e5
GabrielAF-Faca <gabrielafachim2@gmail.com>
2025-08-03 13:55:47 -0300

    ataque funcionando

--- scripts/ground_attack.gd
+++ scripts/ground_attack.gd
@@ -3,9 +3,8 @@ extends Node2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	
-	animated_sprite_2d.play("default")
-
+	#animated_sprite_2d.play("default")
+	$AnimationPlayer.play("activate_hit")
 
 func set_properties(pos: Vector2):
 	global_position = pos
