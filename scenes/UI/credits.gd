515041886e682ade74fdf269d376b8e9562049be
Alisson Lamarque <alissonlamarque.si@gmail.com>
2025-08-03 08:13:17 -0300

    Estilo telas de menu

--- scenes/UI/credits.gd
+++ scenes/UI/credits.gd
@@ -1,4 +1,10 @@
 extends Control
 
+@export var back_button: Button
+
+func _ready():
+	if back_button:
+		back_button.grab_focus()
+
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
